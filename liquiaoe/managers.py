fd6184bf8f4e6f976d0ed23625f25ae3f34d159f
porcpine <porcpine@gmail.com>
2022-01-29 18:29:29 -0500

    Refactor manager code

--- liquiaoe/managers.py
+++ liquiaoe/managers.py
@@ -261,5 +261,23 @@ class Tournament:
         except (AttributeError, IndexError):
             pass
 
+class PlayerManager:
+    def __init__(self, loader):
+        self.loader = loader
+
+    def tournaments(self, player_url):
+        player_tournaments = []
+        if "index" in player_url:
+            return player_tournaments
+        url = "{}/Results".format(player_url)
+        data = self.loader.soup(url)
+        results_table = node_from_class(data, "wikitable")
+        for node in results_table.descendants:
+            if node.name == "tr" and len(node.find_all("td")) == 11:
+                tournament = Tournament()
+                tournament.load_from_player(node)
+                player_tournaments.append(tournament)
+        return player_tournaments
+        
 class ParserError(Exception):
     """ What to throw if something critical missing from soup."""
--- tests/test_managers.py
+++ tests/test_managers.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 from datetime import date
 import pytest
-from liquiaoe.managers.tournament_manager import TournamentManager
+from liquiaoe.managers import TournamentManager, PlayerManager
 from liquiaoe.loaders import VcrLoader
 
 
 @pytest.fixture
 def tournament_manager():
     return TournamentManager(VcrLoader())
+
+@pytest.fixture
+def player_manager():
+    return PlayerManager(VcrLoader())
 
 def print_info(tournamentdict):
     for game, tournaments in tournamentdict.items():
@@ -18,6 +22,18 @@ def print_info(tournamentdict):
             print("{:25}: {} - {}".format(tournament.name,
                                       tournament.start,
                                       tournament.end))
+
+def test_viper(player_manager):
+    """ Tests the viper's page."""
+    viper_url = "/ageofempires/TheViper"
+    tournaments = player_manager.tournaments(viper_url)
+    assert len(tournaments) == 214
+
+def test_trundo(player_manager):
+    trundo_url = "/ageofempires/index.php?title=Trundo&action=edit&redlink=1"
+    
+    tournaments = player_manager.tournaments(trundo_url)
+    assert len(tournaments) == 0
 
 def test_completed(tournament_manager):
     """ Tests tournament filter."""
